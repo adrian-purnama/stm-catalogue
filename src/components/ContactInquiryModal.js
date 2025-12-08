621cdf2de6aa1f51cf16fea8804ae63712e7bda4
ADR-ian <119506352+ADR-ian-ba@users.noreply.github.com>
2025-12-08 09:03:55 +0700

    fix public api url

--- src/components/ContactInquiryModal.js
+++ src/components/ContactInquiryModal.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { X, Loader2, Mail } from 'lucide-react';
 
 //const API_URL = process.env.NEXT_PUBLIC_API_URL;
-const API_URL = 'https://uat-stm-portal-be.stm-asb.co.id/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL
 
 export default function ContactInquiryModal({ isOpen, onClose }) {
   const [inquiryType, setInquiryType] = useState('personal'); // 'personal' or 'company'
